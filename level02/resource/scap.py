32523f4b43917a4ccaefbac7a1eacc7196891047
yoan garrot <garrot.yoan1@gmail.com>
2020-01-08 13:40:19 +0100

    fix typos

--- level02/resource/scap.py
+++ level02/resource/scap.py
@@ -5,10 +5,9 @@ st = []
 for i, packet in enumerate(p):
     if i > 43 and packet[TCP].haslayer(Raw):
         b = str(packet[TCP][Raw].load, 'utf-8')
-        if b.isalnum():
+        if b.isprintable():
             st.append(b)
         elif ord(b[0]) == 0x7f:
             st = st[:-1]
-        #print(str(packet[TCP][Raw].load))
 print(''.join(st))
 #ft_waNDReL0L
